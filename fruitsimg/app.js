3edbd3cf159fa3018a5ba268e51b0f8aa5c33d9b
JaanviNL <rdbhojwani93@gmail.com>
2022-01-26 10:05:42 +0530

    Update app.js

--- fruitsimg/app.js
+++ fruitsimg/app.js
@@ -1,8 +1,9 @@
-
-
+console.log("hello");
+/*
 var getvalue = document.getElementsByName("fruit")[0];
 
 getvalue.addEventListener("input", function () {
   console.log(this.value);
   document.querySelector(".display").innerHTML = this.value; 
 });
+*/
